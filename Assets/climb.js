747350a360e0e0dd2c206467aa779866b26aabe8
Ben Ahlbrand <bmahlbrand@gmail.com>
2015-03-17 19:24:15 -0400

    ladder working

--- Assets/climb.js
+++ Assets/climb.js
@@ -1,18 +1,15 @@
- function OnTriggerEnter (other : Collider) 
-    { 
-        if(other.gameObject.name == "ladder")
-        {
-            transform.GetComponent(CharacterController).slopeLimit = 100; 
-            transform.GetComponent("CharacterMotor").sliding.enabled = false;
-        }
-    } 
- 
-    function OnTriggerExit (other : Collider) 
-    { 
-        if(other.gameObject.name == "ladder")
-        {
-            print("leaving ladder");
-            transform.GetComponent(CharacterController).slopeLimit = 45; 
-            transform.GetComponent("CharacterMotor").sliding.enabled = true;
-        }
+ function OnTriggerEnter (other : Collider) { 
+    if(other.gameObject.CompareTag("Ladder")) {
+    	print("getting on ladder");	
+        transform.GetComponent(CharacterController).slopeLimit = 100; 
+        transform.GetComponent("CharacterMotor").sliding.enabled = false;
     }
+} 
+ 
+function OnTriggerExit (other : Collider) { 
+    if(other.gameObject.CompareTag("Ladder")) {
+        print("leaving ladder");
+        transform.GetComponent(CharacterController).slopeLimit = 45; 
+        transform.GetComponent("CharacterMotor").sliding.enabled = true;
+    }
+}
--- Assets/scripts/enemyfollow.js
+++ Assets/scripts/enemyfollow.js
@@ -1,33 +1,21 @@
 var Player : Transform;
-
- static var MoveSpeed = 4;
+static var MoveSpeed = 4;
 
 var MaxDist = 10;
 var MinDist = 0;
  
-function Start () 
-{
+function Start () {
 
 }
 
-function Update () 
-{
- 
-
- var Player = GameObject.FindWithTag("MainCamera").transform;
-        transform.LookAt(Player.transform);
+function Update () {
+	var Player = GameObject.FindWithTag("MainCamera").transform;
+    transform.LookAt(Player.transform);
  
     if(Vector3.Distance(transform.position,Player.position) >= MinDist){
- 
          transform.position += transform.forward*MoveSpeed*Time.deltaTime;
- 
-  
-         if(Vector3.Distance(transform.position,Player.position) <= MaxDist)
-             {Debug.Log("MoveSpeed");
-                //Here Call any function U want Like Shoot at here or something
-
-   } 
-
-	
-   }
- }	
+         if(Vector3.Distance(transform.position,Player.position) <= MaxDist) {
+             //Debug.Log("MoveSpeed");
+   		}
+   	}
+}
